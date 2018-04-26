037fdcf8643377e65f535e8956c594b5914a5294
Chaster Mateo <chaster@hawaii.edu>
2018-04-26 09:48:19 -1000

    vendors-components-added

---
+++ app/imports/ui/components/vendorHome.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Card, Image, Feed } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter, Link } from 'react-router-dom';
+import Note from '/imports/ui/components/Note';
+import AddNote from '/imports/ui/components/AddNote';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class VendorsHome extends React.Component {
+  render() {
+    return (
+        <Card centered>
+          <Card.Content>
+            <Image floated='right' size='mini' src={this.props.vendor.image}/>
+            <Card.Header>
+              {this.props.vendor.Name}
+            </Card.Header>
+            <Card.Meta>
+              {this.props.vendor.type}
+            </Card.Meta>
+            <Card.Description>
+              {this.props.vendor.description}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Link to={`/edit/${this.props.vendor._id}`}>Edit</Link>
+          </Card.Content>
+          <Card.Content extra>
+            <Feed>
+              {this.props.notes.map((note, index) => <Note key={index} note={note}/>)}
+            </Feed>
+          </Card.Content>
+          <Card.Content extra>
+            <AddNote owner={this.props.vendor.owner} vendorId={this.props.vendor._id}/>
+          </Card.Content>
+        </Card>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+VendorsHome.propTypes = {
+  vendor: PropTypes.object.isRequired,
+  notes: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(VendorsHome);
--- app/imports/ui/pages/vendorHomepage.jsx
+++ app/imports/ui/pages/vendorHomepage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Container, Header, Loader, Card } from 'semantic-ui-react';
-import { Contacts } from '/imports/api/contact/contact';
+import { Vendors } from '/imports/api/vendor/vendor';
 import { Notes } from '/imports/api/note/note';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
-import Contact from '/imports/ui/components/Contact';
+import Vendor from '/imports/ui/components/vendorHome';
 
-/** Renders a table containing all of the contact documents. Use <contactItem> to render each row. */
+/** Renders a table containing all of the vendor documents. Use <vendorItem> to render each row. */
 class VendorHomepage extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
@@ -21,19 +21,19 @@ class VendorHomepage extends React.Component {
         <Container>
           <Header as="h2" textAlign="center" inverted>Vendors Home</Header>
           <Card.Group>
-            {this.props.contacts.map((contact, index) =>
-                <Contact key={index}
-                         contact={contact}
-                         notes={this.props.notes.filter(note => (note.contactId === contact._id))}/>)}
+            {this.props.vendors.map((vendor, index) =>
+                <Vendor key={index}
+                         vendor={vendor}
+                         notes={this.props.notes.filter(note => (note.vendorId === vendor._id))}/>)}
           </Card.Group>
         </Container>
     );
   }
 }
 
-/** Require an array of contact documents in the props. */
+/** Require an array of vendor documents in the props. */
 VendorHomepage.propTypes = {
-  contacts: PropTypes.array.isRequired,
+  vendors: PropTypes.array.isRequired,
   notes: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
 };
@@ -41,10 +41,10 @@ VendorHomepage.propTypes = {
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
   // Get access to Stuff documents.
-  const subscription = Meteor.subscribe('Contacts');
+  const subscription = Meteor.subscribe('Vendors');
   const subscription2 = Meteor.subscribe('Notes');
   return {
-    contacts: Contacts.find({}).fetch(),
+    vendors: Vendors.find({}).fetch(),
     notes: Notes.find({}).fetch(),
     ready: subscription.ready() && subscription2.ready(),
   };
