01bc437ee78a0263cbd251adf114bbf5ea87f7e1
brianhoole <hoole@hawaii.edu>
2018-04-26 16:34:14 -1000

    Map Render working

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, Icon, Header } from 'semantic-ui-react';
+import { Grid, Icon, Header, Embed, Container } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -25,11 +25,15 @@ class Landing extends React.Component {
 
             <Grid.Column textAlign='center'>
               <Icon size='huge' inverted name='search'/>
-              <Header as='h1' inverted>Browse Food Categories</Header>
+              <Header as='h1' inverted>Browse Food</Header>
               <Header as='h3' inverted>Craving Sushi, Pho, Pizza, or want to try something new? Search by food
                 categories.</Header>
             </Grid.Column>
           </Grid>
+            <Container>
+          <Embed
+              url="https://www.google.com/maps/embed?pb=!1m12!1m8!1m3!1d5257.194777602245!2d-157.8200138283992!3d21.29528126714656!3m2!1i1024!2i768!4f13.1!2m1!1sRestaurants!5e0!3m2!1sen!2sus!4v1524794176413"/>
+            </Container>
         </div>
     );
   }
