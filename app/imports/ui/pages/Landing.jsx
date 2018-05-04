f19ed1aeb4480405089eaaafdd1b7f5c38709523
brianhoole <hoole@hawaii.edu>
2018-05-03 23:35:30 -1000

    Twitter up

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -8,7 +8,6 @@ class Landing extends React.Component {
         <div className='back-image'>
           <div className ='mapContainer' align="center">
             <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1z9qHzKI8oEIKgsKoIA4vdEW3TU9lJUyQ" width="1040" height="680"></iframe>
-
             <a class="twitter-timeline" data-height="500" data-width="1040"  href="https://twitter.com/GoGrubn/lists/feed?ref_src=twsrc%5Etfw"></a>
           </div>
         </div>
