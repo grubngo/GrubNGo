a20469a50b2eff0e7588ba1acf2942a7edd81c97
victoria-soto <vjaramil@hawaii.edu>
2018-05-04 21:55:48 -1000

    fix formatting
    landing with map and twitter feed

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -11,19 +11,20 @@ class NavBar extends React.Component {
   render() {
     const menuStyle = { marginBottom: '10px' };
     return (
-        <Menu style={menuStyle} attached="top" borderless inverted color={'black'}>
+        <Menu style={menuStyle} attached="top" borderless inverted color="#114A37">
           <Menu.Item as={NavLink} activeClassName="" exact to="/">
-            <Header inverted as='h1'>Grub N' Go</Header>
+            <Header inverted as='h1'>Grub 'N' Go</Header>
           </Menu.Item>
           {this.props.currentUser ? (
-              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Vendor</Menu.Item >,
+              [<Menu.Item as={NavLink} activeClassName="active" exact to="/add" key='add'>Add Vendor</Menu.Item>,
                 <Menu.Item as={NavLink} activeClassName="active" exact to="/list" key='list'>List Vendors</Menu.Item>]
           ) : ''}
           {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-            <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
-        ) : ''}
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Admin</Menu.Item>
+          ) : ''}
           {Roles.userIsInRole(Meteor.userId(), 'vendor') ? (
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/vendor" key='vendor'>Vendor HomePage</Menu.Item>
+              <Menu.Item as={NavLink} activeClassName="active" exact to="/vendor" key='vendor'>Vendor
+                HomePage</Menu.Item>
           ) : ''}
           <Menu.Item position="right">
             {this.props.currentUser === '' ? (
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
-import { Grid, Icon, Header, Embed, Container } from 'semantic-ui-react';
+import { Header, Grid } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
         <div className='back-image'>
-          <div className ='mapContainer' align="center">
-            <Header as='h1' inverted>Find your GRUB!!!</Header>
-            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1z9qHzKI8oEIKgsKoIA4vdEW3TU9lJUyQ" width="1040" height="680"></iframe>
-            <Header as='h1' inverted>Where to GO!!!</Header>
-            <a class="twitter-timeline" data-height="500" data-width="1040"  href="https://twitter.com/GoGrubn/lists/feed?ref_src=twsrc%5Etfw"></a>
-          </div>
+
+          {/** capitalize GRID!!!! */}
+          <Grid container stackable columns={2}>
+            <Grid.Column inverted>
+              <Header as='h1' inverted>Live updates from restaurants at UH Manoa!</Header>
+              <a className='twitter-timeline' data-height='680' data-width='300'
+                 href="https://twitter.com/GoGrubn/lists/feed?ref_src=twsrc%5Etfw"></a>
+            </Grid.Column>
+
+            <Grid.Column inverted>
+              <Header as='h1' inverted>Hungry at UH Manoa? Use the map below to getchyo GRUB on!</Header>
+              <iframe
+                  width="1040"
+                  height="680"
+                  src="https://www.google.com/maps/d/u/0/embed?mid=1z9qHzKI8oEIKgsKoIA4vdEW3TU9lJUyQ">
+              </iframe>
+            </Grid.Column>
+          </Grid>
         </div>
     );
   }
